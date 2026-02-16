58cc0e9ffdbcb879e8405059019fb2f209d63758
hoge1e3 <hoge1e3@gmail.com>
2026-02-16 17:44:34 +0900

    minimal diff

--- src/changeEvent.ts
+++ src/changeEvent.ts
@@ -1,18 +1,18 @@
-export type ChangeEvent={key: string, value: string | null};
-export class ChangeEventTrait {
+export type ChangeEvent<T>={key: string, value: T | null};
+export class ChangeEventTrait<T> {
     constructor(public target:any){}
-    private listeners: Set<(e:ChangeEvent) => void> = new Set();    
-    addEventListener(type:"change",callback: (e:ChangeEvent) => void): void {
+    private listeners: Set<(e:ChangeEvent<T>) => void> = new Set();    
+    addEventListener(type:"change",callback: (e:ChangeEvent<T>) => void): void {
         this.listeners.add(callback);
     }
-    removeEventListener(callback: (e:ChangeEvent) => void): void {
+    removeEventListener(callback: (e:ChangeEvent<T>) => void): void {
         this.listeners.delete(callback);
     }
-    notifyListeners(key: string, value: string | null): void {
+    notifyListeners(key: string, value: T | null): void {
         this.listeners.forEach(callback => callback.call(this.target,{key, value}));
     }
-    bypass(source: ChangeEventTrait): void {
-        source.addEventListener("change", (e: ChangeEvent) => {
+    bypass(source: ChangeEventTrait<T>): void {
+        source.addEventListener("change", (e: ChangeEvent<T>) => {
             this.notifyListeners(e.key, e.value);
         });
     }   
